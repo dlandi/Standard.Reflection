d10e732e968e4aecae41439d0e0a43c88f3dc298
Brian Parker <brian.parker@hotmail.com.au>
2023-04-29 01:14:35 +1000

    BROKERS: Properties

---
+++ Standard.Reflection/Brokers/Properties/PropertyInfoBroker.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Reflection;
+using System;
+
+namespace Standard.Reflection.Brokers.Properties
+{
+    internal class PropertyInfoBroker : IPropertyInfoBroker
+    {
+        public PropertyInfo[] GetProperties(Type type) =>
+           type.GetProperties();
+    }
+}
