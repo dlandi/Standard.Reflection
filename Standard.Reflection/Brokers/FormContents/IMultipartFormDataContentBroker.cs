56269efb4a889573d8f3f7f4fa6c648e2354f020
Brian Parker <brian.parker@hotmail.com.au>
2023-04-29 02:32:52 +1000

    BROKERS: MultipartFormDataContent

---
+++ Standard.Reflection/Brokers/FormContents/IMultipartFormDataContentBroker.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Net.Http;
+
+namespace Standard.Reflection.Brokers.FormContents
+{
+    internal interface IMultipartFormDataContentBroker
+    {
+        MultipartFormDataContent AddStreamContent(
+            MultipartFormDataContent multipartFormDataContent,
+            Stream stream,
+            string name);
+
+        MultipartFormDataContent AddStreamContent(
+            MultipartFormDataContent multipartFormDataContent,
+            string content,
+            string name,
+            string fileName);
+
+        MultipartFormDataContent AddStringContent(
+            MultipartFormDataContent multipartFormDataContent,
+            string content,
+            string name);
+    }
+}
---
+++ Standard.Reflection/Brokers/FormContents/MultipartFormDataContentBroker.cs
@@ -0,0 +1,46 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Net.Http;
+
+namespace Standard.Reflection.Brokers.FormContents
+{
+    internal class MultipartFormDataContentBroker : IMultipartFormDataContentBroker
+    {
+        public MultipartFormDataContent AddStringContent(
+            MultipartFormDataContent multipartFormDataContent,
+            string content,
+            string name)
+        {
+            var stringContent = new StringContent(content);
+            multipartFormDataContent.Add(stringContent, name);
+
+            return multipartFormDataContent;
+        }
+
+        public MultipartFormDataContent AddStreamContent(
+            MultipartFormDataContent multipartFormDataContent,
+            Stream stream,
+            string name)
+        {
+            var streamContent = new StreamContent(stream);
+            multipartFormDataContent.Add(streamContent, name);
+
+            return multipartFormDataContent;
+        }
+
+        public MultipartFormDataContent AddStreamContent(
+            MultipartFormDataContent multipartFormDataContent,
+            string content,
+            string name,
+            string fileName)
+        {
+            var stringContent = new StringContent(content);
+            multipartFormDataContent.Add(stringContent, name, fileName);
+
+            return multipartFormDataContent;
+        }
+    }
+}
