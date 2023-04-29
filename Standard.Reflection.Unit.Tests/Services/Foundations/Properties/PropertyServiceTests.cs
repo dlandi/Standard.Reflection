fc027e8f5cabdeb9139d084c2e1976970152a9ea
Brian Parker <brian.parker@hotmail.com.au>
2023-04-29 15:35:06 +1000

    ShouldRetrieveProperties -> FAIL

---
+++ Standard.Reflection.Unit.Tests/Services/Foundations/Properties/PropertyServiceTests.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Reflection;
+using Moq;
+using Standard.Reflection.Brokers.Properties;
+using Standard.Reflection.Services.Foundations.Properties;
+
+namespace Standard.Reflection.Unit.Tests.Services.Foundations.Properties
+{
+    public partial class PropertyServiceTests
+    {
+        private readonly Mock<IPropertyBroker> propertyBrokerMock;
+        private readonly IPropertyService propertyService;
+
+        public PropertyServiceTests()
+        {
+            this.propertyBrokerMock = new Mock<IPropertyBroker>();
+            this.propertyService = new PropertyService(this.propertyBrokerMock.Object);
+        }
+
+        private static PropertyInfo[] CreateRandomProperties()
+        {
+            int randomPropertyCount = GetRandomNumber();
+
+            PropertyInfo[] properties = Enumerable.Range(start: 0, count: randomPropertyCount)
+                .Select(i => new Mock<PropertyInfo>().Object)
+                    .ToArray();
+
+            return properties;
+        }
+
+        private static int GetRandomNumber() =>
+            Random.Shared.Next(minValue: 3, maxValue: 8);
+    }
+}
---
+++ Standard.Reflection/Services/Foundations/Properties/PropertyService.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using Standard.Reflection.Brokers.Properties;
+
+namespace Standard.Reflection.Services.Foundations.Properties
+{
+    internal class PropertyService : IPropertyService
+    {
+        private readonly IPropertyBroker propertyBroker;
+
+        public PropertyService(IPropertyBroker propertyBroker) =>
+            this.propertyBroker = propertyBroker;
+
+        public PropertyInfo[] RetrieveProperties(Type type) =>
+            throw new NotImplementedException();
+    }
+}
