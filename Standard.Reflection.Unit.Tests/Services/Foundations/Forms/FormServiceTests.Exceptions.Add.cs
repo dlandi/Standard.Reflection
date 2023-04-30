b72f2da94d52035e75095cd28483d861845035b7
Brian Parker <brian.parker@hotmail.com.au>
2023-05-01 09:17:28 +1000

    ShouldThrowFormServiceExceptionIfExceptionOccurs -> FAIL

--- Standard.Reflection.Unit.Tests/Services/Foundations/Forms/FormServiceTests.Exceptions.Add.cs
+++ Standard.Reflection.Unit.Tests/Services/Foundations/Forms/FormServiceTests.Exceptions.Add.cs
@@ -5,6 +5,7 @@
 using FluentAssertions;
 using Moq;
 using Standard.Reflection.Models.Foundations.Forms.Exceptions;
+using Standard.Reflection.Unit.Tests.Services.Foundations.Properties;
 using System;
 using System.IO;
 using System.Net.Http;
@@ -54,30 +55,43 @@ namespace Standard.Reflection.Unit.Tests.Services.Foundations.Forms
             this.multipartFormDataContentBroker.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public void ShouldThrowFormServiceExceptionIfExceptionOccurs()
+        {
+            // given
+            var multipartFormDataContent = new MultipartFormDataContent();
+            var expectedMultipartFormDataContent = new MultipartFormDataContent();
+            Stream someContent = CreateSomeStreamContent();
+            string randomName = CreateRandomString();
+
+            var someException = new Exception();
+
+            var failedFormServiceException =
+                new FailedFormServiceException(someException);
+
+            var expectedFormServiceException =
+                new FormServiceException(failedFormServiceException);
+
+            this.multipartFormDataContentBroker.Setup(broker =>
+                broker.AddStreamContent(It.IsAny<MultipartFormDataContent>(), It.IsAny<Stream>(), It.IsAny<string>()))
+                    .Throws(someException);
+
+            // when
+            Action retrieveAttributeAction =
+                () => this.formService.AddStreamContent(multipartFormDataContent, someContent, randomName);
+
+            FormServiceException actualFormServiceException =
+                  Assert.Throws<FormServiceException>(retrieveAttributeAction);
+
+            // then
+            actualFormServiceException.Should()
+                .BeEquivalentTo(expectedFormServiceException);
+
+            this.multipartFormDataContentBroker.Verify(broker =>
+                broker.AddStreamContent(It.IsAny<MultipartFormDataContent>(), It.IsAny<Stream>(), It.IsAny<string>()),
+                    Times.Once());
+
+            this.multipartFormDataContentBroker.VerifyNoOtherCalls();
+        }
     }
 }
-
-
-//// given
-//var multipartFormDataContent = new MultipartFormDataContent();
-//var expectedMultipartFormDataContent = new MultipartFormDataContent();
-//Stream someContent = CreateSomeStreamContent();
-//string randomName = CreateRandomString();
-
-//this.multipartFormDataContentBroker.Setup(broker =>
-//    broker.AddStreamContent(multipartFormDataContent, someContent, randomName))
-//        .Returns(expectedMultipartFormDataContent);
-
-//// when
-//var actualMultipartFormDataContent =
-//    formService.AddStreamContent(multipartFormDataContent, someContent, randomName);
-
-//// then
-//actualMultipartFormDataContent.Should()
-//    .BeSameAs(expectedMultipartFormDataContent);
-
-//this.multipartFormDataContentBroker.Verify(broker =>
-//    broker.AddStreamContent(multipartFormDataContent, someContent, randomName),
-//        Times.Once);
-
-//this.multipartFormDataContentBroker.VerifyNoOtherCalls();
---
+++ Standard.Reflection/Models/Foundations/Forms/Exceptions/FormServiceException.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standard.Reflection.Models.Foundations.Forms.Exceptions
+{
+    public class FormServiceException : Xeption
+    {
+        public FormServiceException(Xeption innerException)
+            : base(message: "Form service error occurred, contact support.", innerException)
+        { }
+    }
+}
