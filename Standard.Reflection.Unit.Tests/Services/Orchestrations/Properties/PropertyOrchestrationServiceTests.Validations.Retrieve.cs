0184fb7cf32ecf324846bacdc4d51d9f61740aba
Brian Parker <brian.parker@hotmail.com.au>
2023-05-01 19:10:14 +1000

    ShouldThrowPropertyOrchestrationDependencyValidationExceptionOnRetrievePropertiesIfNullPropertyModelOccurs -> FAIL

---
+++ Standard.Reflection.Unit.Tests/Services/Orchestrations/Properties/PropertyOrchestrationServiceTests.Validations.Retrieve.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Moq;
+using Standard.Reflection.Models.Orchestrations.Properties;
+using Standard.Reflection.Models.Orchestrations.Properties.Exceptions;
+using Xunit;
+
+namespace Standard.Reflection.Unit.Tests.Services.Orchestrations.Properties
+{
+    public partial class PropertyOrchestrationServiceTests
+    {
+        [Fact]
+        public void ShouldThrowPropertyOrchestrationDependencyValidationExceptionOnRetrievePropertiesIfNullPropertyModelOccurs()
+        {
+            // given
+            PropertyModel nullPropertyModel = null;
+            PropertyModel inputPropertyModel = nullPropertyModel;
+            Type someType = typeof(object);
+
+            var argumentNullException =
+                new ArgumentNullException(paramName: "propertyModel");
+
+            var nullPropertyModelException =
+                new NullPropertyModelException(argumentNullException);
+
+            var expectedPropertyOrchestrationDependencyValidationException =
+                new PropertyOrchestrationDependencyValidationException(nullPropertyModelException);
+
+            this.typeServiceMock.Setup(service =>
+                service.RetrieveType(It.IsAny<PropertyModel>()))
+                    .Returns(someType);
+
+            // when
+            Action retrievePropertiesAction = () => this.propertyOrchestrationService.RetrieveProperties(inputPropertyModel);
+
+
+            PropertyOrchestrationDependencyValidationException actualPropertyOrchestrationDependencyValidationException =
+                Assert.Throws<PropertyOrchestrationDependencyValidationException>(retrievePropertiesAction);
+
+
+            // then
+            actualPropertyOrchestrationDependencyValidationException.Should().BeEquivalentTo(expectedPropertyOrchestrationDependencyValidationException);
+
+            this.typeServiceMock.Verify(service =>
+                service.RetrieveType(It.IsAny<PropertyModel>()),
+                    Times.Once());
+
+            this.propertyServiceMock.Verify(service =>
+                service.RetrieveProperties(someType),
+                    Times.Never());
+
+            this.typeServiceMock.VerifyNoOtherCalls();
+            this.propertyServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standard.Reflection/Models/Orchestrations/Properties/Exceptions/PropertyOrchestrationDependencyValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace Standard.Reflection.Models.Orchestrations.Properties.Exceptions
+{
+    public class PropertyOrchestrationDependencyValidationException : Xeption
+    {
+        public PropertyOrchestrationDependencyValidationException(Exception innerException)
+            : base(message: "Property dependency validation error occurred, fix errors and try again.", innerException)
+        { }
+    }
+}
